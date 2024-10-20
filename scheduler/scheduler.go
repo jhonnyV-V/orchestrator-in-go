76c29664a268be0aedee4ec6b5611c4419b23748
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2024-10-19 22:09:44 -0400

    scheduler struct

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,17 @@
 package scheduler
 
+import (
+	"github.com/jhonnyV-V/orch-in-go/node"
+	"github.com/jhonnyV-V/orch-in-go/task"
+)
+
 type Scheduler interface {
-	SelectCandidateNodes()
-	Score()
-	Pick()
+	SelectCandidateNodes(t task.Task, nodes []*node.Node) *node.Node
+	Score(t task.Task, nodes []*node.Node) map[string]float64
+	Pick(scores map[string]float64, candidates []*node.Node) *node.Node
+}
+
+type RoundRobin struct {
+	Name       string
+	LastWorker int
 }
